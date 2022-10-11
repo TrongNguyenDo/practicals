b363591f6619f8b112ae55c1dbb3d111030b3353
nguyeniscoding <95200776+nguyeniscoding@users.noreply.github.com>
2022-10-11 16:08:57 +1000

    complete 4 points of state_name.py

--- prac_05/state_name.py
+++ prac_05/state_name.py
@@ -5,14 +5,26 @@ File needs reformatting
 """
 
 # TODO: Reformat this file so the dictionary code follows PEP 8 convention
-CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
-            "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
-print(CODE_TO_NAME)
+CODE_TO_NAME = {
+    "QLD": "Queensland",
+    "NSW": "New South Wales",
+    "NT": "Northern Territory",
+    "WA": "Western Australia",
+    "ACT": "Australian Capital Territory",
+    "VIC": "Victoria",
+    "TAS": "Tasmania"
+}
 
-state_code = input("Enter short state: ")
+print(CODE_TO_NAME)
+state_code = input("Enter short state:(hit enter to exit): ").upper()
 while state_code != "":
-    if state_code in CODE_TO_NAME:
+    try:
+       if state_code in CODE_TO_NAME:  # find key
         print(state_code, "is", CODE_TO_NAME[state_code])
-    else:
+    except ValueError:
         print("Invalid short state")
-    state_code = input("Enter short state: ")
+    state_code = input("Enter short state: ").upper()
+print("Bye")
+
+for state_code in CODE_TO_NAME:
+    print(f"{state_code:3} is {CODE_TO_NAME[state_code]}")
