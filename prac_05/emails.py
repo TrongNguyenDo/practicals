6c297ac751a8a03311115af2405ec5574bac1bc8
nguyeniscoding <95200776+nguyeniscoding@users.noreply.github.com>
2022-10-13 00:16:33 +1000

    unfinished

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -1,1 +1,24 @@
-print("email.py")
+def main():
+    # lindsay.ward@jcu.edu.au
+    email = input("Please enter your email: ")
+    email_name = {}
+    while email != "":
+        get_name = extract_name(email)
+        confirmation = input(f"Is your name {get_name}? (Y/n) ")
+        if confirmation.upper() != "Y" and confirmation != "":
+            get_name = input("Name: ")
+        extract_name(email)[email] = get_name
+        email = input("Email: ")
+
+    for email, get_name in extract_name(email).items():
+        print(f"{get_name} ({email})")
+
+
+def extract_name(email):
+    change = email.split('@')[0]
+    new_change = change.split('.')
+    get_name = " ".join(new_change).title()
+    return get_name
+
+
+main()
