55c5701e6c538c19a65b022bc77ff0d1b0f324db
Trong Nguyen Do <113566607+TrongNguyenDo@users.noreply.github.com>
2022-11-02 20:31:56 +1000

    Update guitars.py

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -1,37 +1,34 @@
+
 from guitar import Guitar
 
 
 def main():
-    """Guitar program, using Guitar class."""
-    guitar_list = []
+    """Print out Guitars details"""
+    guitars = []
 
     print("My guitars!")
     name = input("Name: ")
     while name != "":
         year = int(input("Year: "))
-        cost = float(input("Cost: "))
-        new_guitar = Guitar(name, year, cost)
-        guitar_list.append(new_guitar)
-        print(new_guitar, "added.")
+        cost = float(input("Cost: $"))
+        guitar_to_add = Guitar(name, year, cost)
+        guitars.append(guitar_to_add)
+        print(guitar_to_add, "added.")
         name = input("Name: ")
 
-        guitar_list.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
-        guitar_list.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
-        guitar_list.append(Guitar("Fender Stratocruisers", 2014, 768.40))
+    guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
+    guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
 
-        if guitar_list:  # lists, strings and other collections are False when empty, True when non-empty
-            # In order for sorting to work on Guitar objects,
-            # at least the __lt__ method must be defined in Guitar class
-            guitar_list.sort()
-            print("These are my guitars:")
-            for i, guitar in enumerate(guitar_list, 1):
-                vintage_string = ""
-                if guitar.is_vintage():
-                    vintage_string = " (vintage)"
-                # Note the use of the format method and numbered placeholders
-                print(
-                    "Guitar {0}: {1.name:>20} ({1.year}), worth ${1.cost:10,.2f}{2}".format(i, guitar, vintage_string))
-        else:
-            print("No guitars :( Quick, go and buy one!")
+    if guitars != "":
+        print("These are my guitars:")
+        for i, guitar in enumerate(guitars, 1):
+            vintage_string = ""
+            if guitar.is_vintage():
+                vintage_string = " (vintage)"
+            # Note the use of the format method and numbered placeholders
+            print("Guitar {0}: {1.name:>20} ({1.year}), worth ${1.cost:10,.2f}{2}".format(i, guitar, vintage_string))
+    else:
+        print("No guitars :( Quick, go and buy one!")
 
-    main()
+
+main()
