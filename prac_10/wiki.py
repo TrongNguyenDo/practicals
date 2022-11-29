b1267ca1fb26a1928f85305351f1b8f079b4c510
Trong Nguyen <trongnguyen.do@my.jcu.edu.au>
2022-11-29 11:28:29 +1000

    Almost finish wiki.py file

---
+++ prac_10/wiki.py
@@ -0,0 +1,20 @@
+import wikipedia
+
+
+def main():
+    while True:
+        search_term = input("Enter a page title or search phrase: ")
+        if search_term == "":
+            break
+        else:
+            wikipedia.search(search_term)
+            print(wikipedia.summary(search_term))
+        # write a try except to catch Disambiguation Error if the user enter Python\
+        while True:
+            try:
+                search_term == "Python"
+            except wikipedia.exceptions.DisambiguationError as e:
+                print(e.options
+
+
+main()
