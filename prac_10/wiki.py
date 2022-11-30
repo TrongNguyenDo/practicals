f0e0d7036e76fec8e5766371469fe3e50a505cfe
Trong Nguyen <trongnguyen.do@my.jcu.edu.au>
2022-11-30 13:26:25 +1000

    Finish the wiki.py file

--- prac_10/wiki.py
+++ prac_10/wiki.py
@@ -2,19 +2,24 @@ import wikipedia
 
 
 def main():
-    while True:
-        search_term = input("Enter a page title or search phrase: ")
-        if search_term == "":
-            break
-        else:
-            wikipedia.search(search_term)
-            print(wikipedia.summary(search_term))
-        # write a try except to catch Disambiguation Error if the user enter Python\
-        while True:
-            try:
-                search_term == "Python"
-            except wikipedia.exceptions.DisambiguationError as e:
-                print(e.options)
+    valid = True
+    """ write a try except to catch Disambiguation Error if the user enter Python"""
+    while not valid:
+        try:
+            search_term = input("Enter a page title or search phrase: ")
+            if search_term == "Python":
+                print("The Python search term could give Disambiguation Error")
+                search_term = input("Enter a page title or search phrase: ")
+                wikipedia.page(search_term)
+                print(wikipedia.summary(search_term))
+            elif search_term != "Python":
+                wikipedia.search(search_term)
+                wikipedia.page(search_term)
+                print(wikipedia.summary(search_term))
+            elif search_term == "":
+                break
+        except wikipedia.exceptions.DisambiguationError as e:
+            print(e.options)
 
 
 main()
